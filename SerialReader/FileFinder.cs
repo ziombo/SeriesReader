3a2d9273a719437a42693fee9fba86677b0504cb
Jan <jasiu@360.st>
2017-04-27 22:22:53 +0200

    Add project files.

---
+++ SerialReader/FileFinder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SerialReader
+{
+    public class FileFinder
+    {
+        public IEnumerable<string> FindMoviesInFileNames(IEnumerable<string> movieNames)
+        {
+            List<string> foundMovieNames = new List<string>();
+
+            // Extract movie name from each file
+            movieNames.ToList().ForEach(mn => foundMovieNames.Add(FindNameInFile(mn)));
+
+
+            foundMovieNames.ForEach(mn => Console.WriteLine(mn));
+            return null;
+        }
+        //todo osobna klasa na sprawdzania plików, dodac do github
+        public IEnumerable<string> FindFilesByExtensions(string directory)
+        {
+            DirectoryInfo d = new DirectoryInfo(directory);//Assuming Test is your Folder
+            IEnumerable<FileInfo> filesAvi = d.GetFiles("*.avi"); //Getting AVI files
+            IEnumerable<FileInfo> filesMp4 = d.GetFiles("*.mp4"); //Getting MP4 files
+            IEnumerable<FileInfo> filesMkv = d.GetFiles("*.mkv"); //Getting MKV files
+
+            List<string> allFiles = new List<string>();
+            allFiles.AddRange(filesAvi.Select(f => f.Name));
+            allFiles.AddRange(filesMp4.Select(f => f.Name));
+            allFiles.AddRange(filesMkv.Select(f => f.Name));
+
+            return allFiles;
+        }
+
+        private string FindNameInFile(string fileName)
+        {
+            Regex regex = new Regex(@".+[S]\d");
+            Match match = regex.Match(fileName);
+            if (match.Success)
+                return match.Value.Remove(match.Value.Length - 3);
+
+            return null;          
+        }
+    }
+}
---
+++ SerialReader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialReader
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            FileFinder fileFinder = new FileFinder();
+            fileFinder.FindMoviesInFileNames(fileFinder.FindFilesByExtensions(@"D:\"));
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
