2197cf8c5c94a093b7421f2799ba9ecdcbd4dfcf
Jan <jasiu@360.st>
2017-05-18 21:55:16 +0200

    Created Interface for unit tests (mocking).

---
+++ SerialReader/GetFilesFromDir.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SerialReader
+{
+    class GetFilesFromDir : IGetFilesFromDir
+    {
+        public IEnumerable<string> GetFiles(string path)
+        {
+            return Directory.GetFiles(path).ToList();
+        }
+    }
+}
---
+++ SerialReader/IGetFilesFromDir.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialReader
+{
+    interface IGetFilesFromDir
+    {
+        IEnumerable<string> GetFiles(string path);
+    }
+}
