4b1d1327caa4c73def7b052dbb3ef8930cf9e4d5
Jan <jasiu@360.st>
2017-11-05 15:24:34 +0100

    Added console coloring when user inputs seriesName

--- SerialReaderConsole/ConsoleSeriesHandler.cs
+++ SerialReaderConsole/ConsoleSeriesHandler.cs
@@ -1,42 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using SerialReaderLibrary.Model;
 using SerialReaderLibrary.Utils.Series.Downloader;
 
 namespace SerialReaderConsole
 {
-    public static class ConsoleSeriesHandler
+    public class ConsoleSeriesHandler
     {
-        public static void DisplaySeriesCollection()
+        private static List<SeriesGeneral> AllSeries = new List<SeriesGeneral>();
+
+
+        public static void DisplayConsoleMenu()
         {
-            throw new NotImplementedException();
+            string command = "";
+            while (command != "3")
+            {
+                Console.WriteLine("Available commands:");
+                Console.WriteLine("1) Download series");
+                Console.WriteLine("2) Display series collection");
+                Console.WriteLine("3) Exit.");
+                command = Console.ReadLine();
+
+                Console.WriteLine();
+
+                LaunchCommand(command);
+            }
         }
 
-        public static void GetSeriesForConsole()
+        private static void LaunchCommand(string command)
         {
+            switch (command)
+            {
+                case "1":
+                    GetSeriesForConsole();
+                    break;
+                case "2":
+                    DisplaySeriesCollection();
+                    break;
+                case "3":
+                    break;
+                default:
+                    Console.WriteLine("Invalid command");
+                    break;
+            }
+        }
+
+        private static void DisplaySeriesCollection()
+        {
+            Console.Clear();
+            WriteToEndOfConsoleLine('_');
+
+            AllSeries.ForEach(DisplaySeriesDetailsToConsole);
+
+            WriteToEndOfConsoleLine('_');
+        }
+
+        private static void GetSeriesForConsole()
+        {
+             //dodać sprawdzenie czy w kolekcji juz nie ma po name
+             // dodać name wpisany żeby porównywać też
             string seriesName = GetSeriesNameFromUser();
 
             SeriesGeneral series = DownloadSeries(seriesName);
 
             DisplaySeriesDetailsToConsole(series);
+
+            if(AllSeries.All(s => s.Name != series.Name))
+                AllSeries.Add(series);
         }
 
         private static string GetSeriesNameFromUser()
         {
             Console.Write("Input series name: ");
-            return Console.ReadLine();
+            return InputToConsoleInColor(ConsoleColor.Cyan);
         }
 
         private static SeriesGeneral DownloadSeries(string seriesName)
         {
-            SeriesDownloader seriesDownloader = new SeriesDownloader();
-
-            return seriesDownloader.GetSeries(seriesName);
+            return new SeriesDownloader().GetSeries(seriesName);
         }
 
         private static void DisplaySeriesDetailsToConsole(SeriesGeneral seriesGeneral)
         {
+            Console.WriteLine();
+
             WriteToConsoleColoured(ConsoleColor.Green, "Series: ");
             Console.WriteLine(seriesGeneral.Name);
 
@@ -51,6 +100,8 @@ namespace SerialReaderConsole
                 WriteToConsoleColoured(ConsoleColor.Green, "Days left: ");
                 Console.WriteLine(Math.Ceiling((DateTime.Parse(seriesGeneral.NextEpisodeDate) - DateTime.Now).TotalDays));
             }
+
+            Console.WriteLine();
         }
 
 
@@ -65,6 +116,23 @@ namespace SerialReaderConsole
                 Console.Write(message);
 
             Console.ForegroundColor = currentColorHolder;
+        }
+
+        private static string InputToConsoleInColor(ConsoleColor color)
+        {
+            ConsoleColor currentColorHolder = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+
+            string input = Console.ReadLine();
+
+            Console.ForegroundColor = currentColorHolder;
+
+            return input;
+        }
+
+        private static void WriteToEndOfConsoleLine(char character)
+        {
+            Console.WriteLine(character.ToString().PadRight(Console.WindowWidth, character));
         }
     }
 }
--- SerialReaderConsole/Program.cs
+++ SerialReaderConsole/Program.cs
@@ -13,56 +13,11 @@ namespace SerialReaderConsole
         {
             // dodać event do SeriesReader żeby rzucało event jak błąd i wtedy na czilku się dopisujesz elo
 
-            var xz = new FileOperations();
-            var test = xz.ReadFromAppData();
-            var test2 = JsonConvert.DeserializeObject<SeriesGeneral>(test);
-            SeriesFinder();
-        }
+            //var xz = new FileOperations();
+            //var test = xz.ReadFromAppData();
+            //var test2 = JsonConvert.DeserializeObject<SeriesGeneral>(test);
 
-
-        private static void SeriesFinder()
-        {
-            string command = "";
-            while (command != "3")
-            {
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine("Available commands:");
-                Console.WriteLine("1) Download series");
-                Console.WriteLine("2) Display series collection");
-                Console.WriteLine("3) Exit.");
-                command = Console.ReadLine();
-
-                Console.WriteLine();
-
-                switch (command)
-                {
-                    case "1":
-                        ConsoleSeriesHandler.GetSeriesForConsole();
-                        break;
-                    case "2":
-                        ConsoleSeriesHandler.DisplaySeriesCollection();
-                        break;
-                    case "3":
-                        break;
-                    default:
-                        Console.WriteLine("Invalid command");
-                        break;
-                }
-            }
-        }
-
-        private static void WriteToConsoleColoured(ConsoleColor color, string message, bool writeLine = false)
-        {
-            ConsoleColor currentColorHolder = Console.ForegroundColor;
-            Console.ForegroundColor = color;
-            
-            if(writeLine)
-                Console.WriteLine(message);
-            else
-                Console.Write(message);
-
-            Console.ForegroundColor = currentColorHolder;
+            ConsoleSeriesHandler.DisplayConsoleMenu();
         }
     }
 }
